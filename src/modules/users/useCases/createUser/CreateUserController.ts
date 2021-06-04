745204633a743d59562009903982c176ec1833ae
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-06-03 22:57:51 -0300

    🎉 Desafio Finalizado!

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -8,6 +8,12 @@ class CreateUserController {
   handle(request: Request, response: Response): Response {
     const { name, email } = request.body;
 
+    if (!name || !email) {
+      return response
+        .status(400)
+        .json({ error: "Fields name or email missing!" });
+    }
+
     try {
       const user = this.createUserUseCase.execute({ email, name });
       return response.status(201).json(user);
